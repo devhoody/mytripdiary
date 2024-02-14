d5651da60f7fe941224089c81620fe3f3154afd1
devhoody <rethnoly@gmail.com>
2024-02-14 21:27:54 +0900

    Init: First commit

---
+++ src/main/java/com/mytripdiary/mydiary/MytripdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mytripdiary.mydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytripdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytripdiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytripdiary/mydiary/MytripdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytripdiary.mydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytripdiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
