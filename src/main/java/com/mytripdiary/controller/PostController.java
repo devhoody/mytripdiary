3b73cef25f5038d0903e2300bef44dceb0e5daef
devhoody <rethnoly@gmail.com>
2024-02-19 17:22:43 +0900

    Feat: Post 등록, 수정 구현

--- src/main/java/com/mytripdiary/controller/PostController.java
+++ src/main/java/com/mytripdiary/controller/PostController.java
@@ -6,8 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,6 +24,52 @@ public class PostController {
         model.addAttribute("posts", list);
         return "/post/posts";
     }
+
+    @GetMapping("add")
+    public String add(Model model){
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "/post/addForm";
+    }
+
+    @PostMapping("add")
+    public String add(@ModelAttribute Post post, Model model, RedirectAttributes redirectAttributes){
+        Post savedPost = postService.save(post);
+        model.addAttribute("post", savedPost);
+
+        redirectAttributes.addAttribute("postId", savedPost.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/posts/{postId}";
+    }
+
+    @GetMapping("{postId}")
+    public String post(@PathVariable(name = "postId") Long id, Model model){
+        Post findedPost = postService.findById(id);
+        model.addAttribute("post", findedPost);
+
+        return "/post/post";
+    }
+
+    @GetMapping("{postId}/edit")
+    public String edit(@PathVariable(name = "postId") Long id, Model model){
+        Post findedPost = postService.findById(id);
+        model.addAttribute("post", findedPost);
+
+        return "/post/editForm";
+    }
+
+    @PostMapping("{postId}/edit")
+    public String edit(@ModelAttribute Post post, Model model, RedirectAttributes redirectAttributes){
+        postService.modify(post.getId(), post);
+        model.addAttribute("post", post);
+
+        redirectAttributes.addAttribute("postId", post.getId());
+
+        return "redirect:/posts/{postId}";
+    }
+
+
 
 
 }
