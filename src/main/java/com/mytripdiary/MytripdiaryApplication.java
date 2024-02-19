17f022dca008a4f4a3d7d87a9e6df97acc2786b3
devhoody <rethnoly@gmail.com>
2024-02-19 15:59:38 +0900

    Rename: Application, ApplicationTest파일 이동

--- src/main/java/com/mytripdiary/MytripdiaryApplication.java
+++ src/main/java/com/mytripdiary/MytripdiaryApplication.java
@@ -1,4 +1,4 @@
-package com.mytripdiary.mydiary;
+package com.mytripdiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mytripdiary/MytripdiaryApplicationTests.java
+++ src/test/java/com/mytripdiary/MytripdiaryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mytripdiary.mydiary;
+package com.mytripdiary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
