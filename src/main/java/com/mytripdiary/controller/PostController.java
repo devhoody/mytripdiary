a8f5c4482ec7eaa8258aa34b83a98ee36d6f3e5d
devhoody <rethnoly@gmail.com>
2024-02-20 16:53:20 +0900

    Feat: 이미지 등록 구현 - controller내 파일 저장 기능 추가

--- src/main/java/com/mytripdiary/controller/PostController.java
+++ src/main/java/com/mytripdiary/controller/PostController.java
@@ -1,39 +1,65 @@
 package com.mytripdiary.controller;
 
 import com.mytripdiary.domain.Post;
+import com.mytripdiary.domain.PostForm;
+import com.mytripdiary.domain.UploadFile;
+import com.mytripdiary.file.FileStore;
 import com.mytripdiary.service.PostServiceImp;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @Controller
 @Slf4j
 @AllArgsConstructor
-@RequestMapping("/posts")
 public class PostController {
     private PostServiceImp postService;
+    private FileStore fileStore;
 
-    @GetMapping("")
-    public String posts(Model model){
+    @GetMapping("posts")
+    public String posts(Model model) {
         List<Post> list = postService.findAll();
         model.addAttribute("posts", list);
         return "/post/posts";
     }
 
-    @GetMapping("add")
-    public String add(Model model){
+    @GetMapping("posts/add")
+    public String add(Model model) {
         Post post = new Post();
         model.addAttribute("post", post);
         return "/post/addForm";
     }
 
-    @PostMapping("add")
-    public String add(@ModelAttribute Post post, Model model, RedirectAttributes redirectAttributes){
+    @PostMapping("posts/add")
+    public String add(@ModelAttribute PostForm form, Model model, RedirectAttributes redirectAttributes) throws IOException {
+
+        MultipartFile attachFile = form.getAttachFile();
+        List<MultipartFile> imageFiles = form.getImageFiles();
+
+        UploadFile uploadFile = fileStore.fileStore(attachFile);
+        List<UploadFile> uploadFiles = fileStore.filesStore(imageFiles);
+
+        log.info("uploadFilename : " + uploadFile.getUploadFilename());
+        log.info("imageFiles : " + uploadFiles.get(0).toString());
+
+        //데이터베이스 저장
+        Post post = Post.builder()
+                .title(form.getTitle())
+                .content(form.getContent())
+                .attachFile(uploadFile)
+                .imageFiles(uploadFiles)
+                .build();
+
         Post savedPost = postService.save(post);
         model.addAttribute("post", savedPost);
 
@@ -43,24 +69,31 @@ public class PostController {
         return "redirect:/posts/{postId}";
     }
 
-    @GetMapping("{postId}")
-    public String post(@PathVariable(name = "postId") Long id, Model model){
+    @ResponseBody
+    @GetMapping("images/{filename}")
+    public Resource image(@PathVariable(name = "filename") String filename) throws MalformedURLException {
+        String fullPath = fileStore.getFullPath(filename);
+        return new UrlResource("file:" + fullPath);
+    }
+
+    @GetMapping("posts/{postId}")
+    public String post(@PathVariable(name = "postId") Long id, Model model) {
         Post findedPost = postService.findById(id);
         model.addAttribute("post", findedPost);
 
         return "/post/post";
     }
 
-    @GetMapping("{postId}/edit")
-    public String edit(@PathVariable(name = "postId") Long id, Model model){
+    @GetMapping("posts/{postId}/edit")
+    public String edit(@PathVariable(name = "postId") Long id, Model model) {
         Post findedPost = postService.findById(id);
         model.addAttribute("post", findedPost);
 
         return "/post/editForm";
     }
 
-    @PostMapping("{postId}/edit")
-    public String edit(@ModelAttribute Post post, Model model, RedirectAttributes redirectAttributes){
+    @PostMapping("posts/{postId}/edit")
+    public String edit(@ModelAttribute Post post, Model model, RedirectAttributes redirectAttributes) {
         postService.modify(post.getId(), post);
         model.addAttribute("post", post);
 
@@ -69,7 +102,7 @@ public class PostController {
         return "redirect:/posts/{postId}";
     }
 
-    @GetMapping("{postId}/delete")
+    @GetMapping("posts/{postId}/delete")
     public String delete(@PathVariable(name = "postId") Long id) {
         postService.delete(id);
         return "redirect:/posts";
